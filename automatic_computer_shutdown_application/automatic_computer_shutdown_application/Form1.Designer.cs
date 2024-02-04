ce7f0814d3080735467d9121f9fd0de598a9ea69
Ahmet <110814262+zapkinus11@users.noreply.github.com>
2024-02-04 23:58:21 +0300

    Automatic_Computer_Shutdown_App
    making an application to turn off your computer after a certain period of time with c#

---
+++ automatic_computer_shutdown_application/automatic_computer_shutdown_application/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace automatic_computer_shutdown_application
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.second = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.second)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label4.Location = new System.Drawing.Point(142, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(380, 22);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "(after how many minutes should it shut down?)";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCancel.Location = new System.Drawing.Point(347, 209);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(119, 70);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnExit.Location = new System.Drawing.Point(251, 299);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(161, 55);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnStart.Location = new System.Drawing.Point(193, 209);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(123, 70);
+            this.btnStart.TabIndex = 11;
+            this.btnStart.Text = "Start the process";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // second
+            // 
+            this.second.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.second.Location = new System.Drawing.Point(266, 140);
+            this.second.Name = "second";
+            this.second.Size = new System.Drawing.Size(125, 37);
+            this.second.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(204, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 37);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Choose duration:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 484);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.second);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Automatic Computer Shutdown";
+            ((System.ComponentModel.ISupportInitialize)(this.second)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown second;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ automatic_computer_shutdown_application/automatic_computer_shutdown_application/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace automatic_computer_shutdown_application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (second.Value > 0)
+            {
+                int closeDuration = (int)second.Value;
+                Process.Start("shutdown", $"/s /t {closeDuration * 60}");
+                MessageBox.Show($"{closeDuration} minute after the computer will shut down!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (second.Value <= 0)
+            {
+                MessageBox.Show("Do not enter the value as empty or missing", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("User problematic error!", "Big error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (second.Value > 0)
+            {
+                Process.Start("shutdown", "/a");
+                MessageBox.Show("Transaction canceled!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (second.Value <= 0)
+            {
+                MessageBox.Show("Enter the time!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Enter the time!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            string computerName = Environment.MachineName;
+            MessageBox.Show($"Thank you for using my application {computerName}", "Ahmet TOPAL's Application", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
+        }
+    }
+}
